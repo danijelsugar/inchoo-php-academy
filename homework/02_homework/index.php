b2ca74f027363bf9802c017cd5f596742f2621bd
Danijel Šugar <danijel.sugar@outlook.com>
2019-01-20 17:26:53 +0100

    Fix: now first bigger number than arithmetic mean is bold

--- homework/02_homework/index.php
+++ homework/02_homework/index.php
@@ -1,45 +1,59 @@
 <?php
 
-    if(isset($_POST['send']) && isset($_POST['nums'])) { //if submit is presed
+    if(isset($_POST['send']) && isset($_POST['nums'])) { //if submit is pressed
         $numbers = ($_POST['nums']); // puts nums send with POST in variable
         $separate = explode(',', $numbers); // removes commas
 
         /*
          * loops through array and removes all non numeric characters,
-         * gets biggest entered number,
-         * gets dimensions of table,
-         * check if number is even and puts it in $even array
-         *
          * */
         foreach ($separate as $num) {
             $num = preg_replace('/[^0-9]/', '', $num);
             $isNumeric[] = $num;
-            $max = max($isNumeric);
-            $bold = array_sum($isNumeric) / count($isNumeric);
-
-            /*
-             * not the best way but it works,
-             * when form is submit empty it still send empty string
-             * so this way it doesnt throw error in sqrt function
-             */
-            if($max === '') {
-                $dimension = 0;
-            } else {
-                $dimension = round(sqrt($max)+1);
-            }
 
 
+            // checking if number is even and puts him in array
             if (is_numeric($num) && $num % 2 === 0) {
                 $even[] = $num;
             }
 
+
+
         }
+
+        $max = max($isNumeric); // gets biggest number from array
+        $bold = array_sum($isNumeric) / count($isNumeric); //arithmetic mean
+
+            /*
+             * not the best way but it works,
+             * when form is submit empty it still send empty string
+             * so this way it doesnt throw error in sqrt function,
+             * gets size of table
+             */
+        if($max === '') {
+            $dimension = 0;
+        } else {
+            $dimension = round(sqrt($max)+1);
+        }
+
+        /*
+         * gets closest even number of arithmetic mean
+         * */
+        $closest = null;
+        foreach ($even as $greater) {
+            if($greater>$bold){
+                $closest = $greater;
+                break;
+            }
+
+        }
+
     }
-    //echo 'Bold: ' . $bold . '<br>';
-    //echo 'Dimension: ' . $dimension . '<br>';
-    echo '<pre>';
+
+
+    //echo '<pre>';
     //print_r($even);
-    echo '</pre>';
+    //echo '</pre>';
 
 
 ?>
@@ -72,19 +86,20 @@
     <hr />
 
 <?php
+
     $k=0;  // counter
     if(isset($_POST['send'])) {
-        echo '<table style="width: 50%; margin: 0 auto;">';
+        echo '<table style="width: 25%; margin: 0 auto;">';
         for ($i = 1; $i <= $dimension; $i++) { //loops for rows
             echo '<tr>';
             for ($j = 1; $j <= $dimension; $j++) {  //loops for columns
                 ++$k;
-                if (!in_array($k, $even)) {
+                if (!in_array($k, $even)) { //if number is not in even array
                     echo '<td>' . ' ' . '</td>';
                 } else {
-                    if($k>$bold) {
-                        echo '<td><b>' . $k . '</b></td>';
-                    } else {
+                    if($k==$closest) { // if is in even array and closest bigger than arithmetic mean
+                        echo '<td><strong>' . $k . '</strong></td>';
+                    } else { // all other even numbers
                         echo '<td>' . $k . '</td>';
                     }
 
